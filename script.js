72c18a5ac22829948374c389f041856f9173dd46
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-16 16:07:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // This code will run when the DOM is fully loaded
+  const paragraph = document.createElement("p"); // Create a new paragraph element
+  paragraph.textContent = "DOM load success"; // Set its text content
+
+  // Append the paragraph to the body of the document
+  document.body.appendChild(paragraph);
+});
